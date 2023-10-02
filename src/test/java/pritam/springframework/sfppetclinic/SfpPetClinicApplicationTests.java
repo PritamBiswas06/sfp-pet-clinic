1168ded1d085241de01164be95c270b18ab1c305
PritamBiswas06 <126695473+PritamBiswas06@users.noreply.github.com>
2023-10-02 12:38:23 +0530

    Initial Project Add

---
+++ src/test/java/pritam/springframework/sfppetclinic/SfpPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package pritam.springframework.sfppetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfpPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
